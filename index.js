aacca2b2f5fb954c460a3ed7a9629115e6e3ca05
topu12345 <topu15-3604@diu.edu.bd>
2023-02-05 13:15:13 +0600

    servar

---
+++ index.js
@@ -0,0 +1,144 @@
+const express = require('express')
+const cors = require('cors');
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dzfwzfk.mongodb.net/?retryWrites=true&w=majority`;
+//console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    const appointmentOPtinoCollection = client.db('doctorsprotal').collection('appointmentOption')
+    const bookingsColleaction = client.db('doctorsprotal').collection('bookings')
+    //use aggregate to quert multiple and then merge data
+   
+    app.get('/appointmentOption', async (req, res) => {
+      const date = req.query.date;
+      const query = {};
+      const options = await appointmentOPtinoCollection.find(query).toArray();
+
+      // get the bookings of the provided date
+      const bookingQuery = { appointmentDate: date }
+      const alreadyBooked = await bookingsColleaction.find(bookingQuery).toArray();
+
+      // code carefully :D
+      options.forEach(option => {
+          const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+          const bookedSlots = optionBooked.map(book => book.slot);
+         // console.log(option.name,bookedSlots)
+          const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+         option.slots = remainingSlots;
+      })
+      res.send(options);
+  });
+
+
+
+//deep mongodb
+
+//   app.get('/v2/appointmentOptions', async (req, res) => {
+//     const date = req.query.date;
+//     const options = await appointmentOptionCollection.aggregate([
+//         {
+//             $lookup: {
+//                 from: 'bookings',
+//                 localField: 'name',
+//                 foreignField: 'treatment',
+//                 pipeline: [
+//                     {
+//                         $match: {
+//                             $expr: {
+//                                 $eq: ['$appointmentDate', date]
+//                             }
+//                         }
+//                     }
+//                 ],
+//                 as: 'booked'
+//             }
+//         },
+//         {
+//             $project: {
+//                 name: 1,
+//                 slots: 1,
+//                 booked: {
+//                     $map: {
+//                         input: '$booked',
+//                         as: 'book',
+//                         in: '$$book.slot'
+//                     }
+//                 }
+//             }
+//         },
+//         {
+//             $project: {
+//                 name: 1,
+//                 slots: {
+//                     $setDifference: ['$slots', '$booked']
+//                 }
+//             }
+//         }
+//     ]).toArray();
+//     res.send(options);
+// })
+
+/***
+ * API Naming Convention 
+ * app.get('/bookings')
+ * app.get('/bookings/:id')
+ * app.post('/bookings')
+ * app.patch('/bookings/:id')
+ * app.delete('/bookings/:id')
+*/
+
+
+
+    //booing
+
+
+  app.post('/bookings', async (req, res) => {
+    const booking = req.body;
+
+
+    console.log(booking);
+    const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment 
+    }
+
+    const alreadyBooked = await bookingsColleaction.find(query).toArray();
+
+    if (alreadyBooked.length){
+        const message = `You already have a booking on ${booking.appointmentDate}`
+        return res.send({acknowledged: false, message})
+    }
+
+    
+
+    const result = await bookingsColleaction.insertOne(booking);
+    res.send(result);
+})
+
+}
+  finally {
+
+  }
+}
+run().catch(console.log)
+
+
+app.get('/', async (req, res) => {
+  res.send('doctors protal servar is running')
+})
+app.listen(port, () => console.log(`Doctors protal running on ${port}`))
